a3782065c25c8a40319188d544404bddd7419e0a
Wilson Bilkovich <wilson@supremetyrant.com>
2010-05-26 14:57:59 -0400

    Clean up "require 'amqp'" behavior

--- lib/amqp.rb
+++ lib/amqp.rb
@@ -1,8 +1,17 @@
-require File.expand_path('../ext/em', __FILE__)
-require File.expand_path('../ext/blankslate', __FILE__)
-  
+unless defined?(EM)
+  begin
+    require 'eventmachine'
+  rescue LoadError
+    require 'rubygems'
+    require 'eventmachine'
+  end
+end
+
+library_root = File.expand_path("..", __FILE__)
+require File.join(library_root, "ext", "emfork")
+require File.join(library_root, "ext", "blankslate")
 %w[ version buffer spec protocol frame client ].each do |file|
-  require File.expand_path("../amqp/#{file}", __FILE__)
+  require File.join(library_root, "amqp", file)
 end
 
 module AMQP
@@ -76,7 +85,7 @@ module AMQP
   # block. See the code examples in MQ for details.
   #
   def self.start *args, &blk
-    EM.run{
+    EM.run {
       @conn ||= connect *args
       @conn.callback(&blk) if blk
       @conn
@@ -86,11 +95,11 @@ module AMQP
   class << self
     alias :run :start
   end
-  
+
   def self.stop
     if @conn and not @closing
       @closing = true
-      @conn.close{
+      @conn.close {
         yield if block_given?
         @conn = nil
         @closing = false
